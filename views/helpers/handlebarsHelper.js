367312fb835f121939a7fd38a963347ae5624d9e
Omar Hammoud <omar.s.hammoud@outlook.com>
2022-09-06 13:30:39 +0300

    added runtime options for issue with "each" helper

--- views/helpers/handlebarsHelper.js
+++ views/helpers/handlebarsHelper.js
@@ -3,6 +3,10 @@ const moment = require('moment');
 
 hbs = handlebars.create({
 	defaultLayout: 'main',
+	runtimeOptions: {
+		allowProtoPropertiesByDefault: true,
+		allowProtoMethodsByDefault: true,
+	},
 	helpers: {
 		isEqual: function (value, name) {
 			if (value == name) {
