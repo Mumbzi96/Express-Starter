e4dbdce529a6a9fad48853b812e3cf26656ebb9b
Omar Hammoud <omar.s.hammoud@outlook.com>
2024-02-14 10:26:05 +0200

    Added cardsList

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -10,7 +10,42 @@ const testRouter = express.Router();
 
 testRouter.get("/cards", (req, res, next) => {
 	let navdata = { language: "English" };
-	res.render("main/test/test", { navdata });
+	let users = [
+		{
+			_id: "1",
+			name: "John Doe",
+			profilePic: "/path/to/profilePic.jpg",
+			phone: {
+				dialCode: "+1",
+				number: "1234567890",
+			},
+			email: "johndoe@example.com",
+			type: "Administrator",
+		},
+		{
+			_id: "2",
+			name: "Jane Smith",
+			// Assuming no profilePic provided, so the default avatar will be used.
+			phone: {
+				dialCode: "+44",
+				number: "0987654321",
+			},
+			email: "janesmith@example.com",
+			type: "User",
+		},
+		{
+			_id: "3",
+			name: "Ahmed Al-Farsi",
+			profilePic: "/path/to/ahmedPic.jpg",
+			phone: {
+				dialCode: "+971",
+				number: "123123123",
+			},
+			email: "ahmedalf@example.com",
+			type: "Manager",
+		},
+	];
+	res.render("main/test/cardsList", { navdata, users });
 });
 
 //========================
